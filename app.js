b15f11ed1a733017c8bfcf13ec4ffed42900a9ab
aydreeyun <uncoadrian@gmail.com>
2020-06-15 10:42:09 -0400

    fixed app.js

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 app.get("/", (req, res) => res.send("Hello World"));
 
