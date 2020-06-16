e3a972c9d0258dde171a224e3b4ed70f47cf2eba
Jonathan <yoniyaniv@gmail.com>
2020-06-16 15:47:16 -0400

    heroku config

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path');
 const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
@@ -31,3 +32,10 @@ app.use("/api/contact_types", contact_types);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
