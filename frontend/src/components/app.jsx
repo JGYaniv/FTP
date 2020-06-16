edd0196aa4fe688254097bd91c8415d32cced82a
William Suh <william.junil.suh@gmail.com>
2020-06-16 09:24:21 -0400

    stylin

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -1,20 +1,21 @@
-// import Messages from './messages/messages_container';
 import Login from './session/login_container';
 import Signup from './session/signup_container';
 import Splash from './splash/splash_container';
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
-
+import NavBarContainer from './nav/navbar_container';
+import Home from './home/home'
 
 export default () => (
   <div>
+    <NavBarContainer />
     <Switch>
       <AuthRoute exact path="/" component={Splash} />
       <AuthRoute exact path="/login" component={Login} />
       <AuthRoute exact path="/signup" component={Signup} />
 
-      {/* <ProtectedRoute exact path="/home" component={Home} /> */}
+      <ProtectedRoute exact path="/home" component={Home} />
     </Switch>
   </div>
 )
---
+++ frontend/src/components/home/messages.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MessagesCSS from './messages.css'
+
+const Messages = () => {
+    return (
+        <div className="messages-container">
+            messages
+        </div>
+    )
+}
+
+export default Messages;
