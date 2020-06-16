1f98a8f4bb44956e44de92c8917b69c932b10924
aydreeyun <uncoadrian@gmail.com>
2020-06-15 23:45:09 -0400

    finished contacts redux cycle

--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import messageReducer from './message_reducer';
+import contactReducer from './contact_reducer';
 
 export default combineReducers({
-  message: messageReducer
+  message: messageReducer,
+  contact: contactReducer
 });
--- frontend/src/reducers/errors_reducer.js
+++ frontend/src/reducers/errors_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import SessionErrorsReducer from './session_errors_reducer';
 import messageErrorsReducer from './message_errors_reducer';
+import contactErrorsReducer from './contact_errors_reducer';
 
 export default combineReducers({
     session: SessionErrorsReducer,
-    message: messageErrorsReducer
+    message: messageErrorsReducer,
+    contact: contactErrorsReducer
 });
