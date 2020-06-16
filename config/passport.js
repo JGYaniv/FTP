28fdf41ebeee6431b9d318f9118142611c3492cd
Jonathan <yoniyaniv@gmail.com>
2020-06-16 16:19:34 -0400

    heroku config3

--- config/passport.js
+++ config/passport.js
@@ -2,11 +2,11 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
-const keys = require('./keys');
+const secretOrKey = process.env.secretOrKey ? process.env.secretOrKey : require('./keys').secretOrKey;
 
 const options = {};
 options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-options.secretOrKey = keys.secretOrKey;
+options.secretOrKey = secretOrKey;
 
 module.exports = passport => {
   passport.use(new JwtStrategy(options, (jwt_payload, done) => {
