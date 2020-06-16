0b415259f69c349ab2141f88d3f1675abff72b42
Jonathan <yoniyaniv@gmail.com>
2020-06-15 23:38:23 -0400

    encryptor module working

---
+++ modules/encryptor.js
@@ -0,0 +1,36 @@
+const encryptor = (cipher) => {
+    const ciphArr = [];
+    
+    for (let i=0; i<cipher.length; i++){
+        let code = cipher.charCodeAt((i+10) % cipher.length)
+        ciphArr.push((code % 10) + 10)
+    }
+
+    return {
+        encrypt: (string) => {
+            var encryptedString = "";
+
+            for (let i=0; i<string.length; i++){
+                let code = (string.charCodeAt(i) + ciphArr[i % ciphArr.length])
+                encryptedString += String.fromCharCode(code)
+            }
+
+            return encryptedString
+        },
+
+        decrypt: (encodedString) => {
+            var encryptedString = "";
+
+            for (let i = 0; i < encodedString.length; i++) {
+                let code = (encodedString.charCodeAt(i) - ciphArr[i % ciphArr.length])
+                encryptedString += String.fromCharCode(code)
+            }
+
+            return encryptedString
+        },
+
+        ciphArr: ciphArr
+    }    
+}
+
+module.exports = encryptor;
